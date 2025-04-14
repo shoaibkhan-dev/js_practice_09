c428834840a30e1c11ed220086e6862238301786
Shoaib khan <shoaibmangal@yahoo.com>
2025-04-15 01:03:36 +0500

    add filed to js practice 09

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-document.getElementById("images").addEventListener('click', function(e){
-    console.log("clicked inside the ul")
-    e.timeStamp()
-}, true)
+// document.getElementById("images").addEventListener('click', function(e){
+//     console.log("clicked inside the ul")
+//     e.timeStamp()
+// }, true)
 
-document.getElementById("shoaib").addEventListener('click', function(e){
-    console.log("clicked shoaib")
-}, true)
+// document.getElementById("shoaib").addEventListener('click', function(e){
+//     console.log("clicked shoaib")
+// }, true)
+
+
+const students = [
+    {id: "A1", name: "Ali"},
+    {id: "A2", name: "Aslam"},
+    {id: "A3", name: "Ahmad"},
+    {id: "A4", name: "shoaib"},
+    {id: "A5", name: "irfan"},
+    {id: "A6", name: "abubakkar"},
+    {id: "A7", name: "umer"},
+    {id: "A8", name: "usman"},
+    {id: "A9", name: "hassan"},
+    {id: "A10", name: "hussain"},
+];
+
+function searchStudent(event){
+  event.preventDefault();
+  const idField = document.getElementById("id-field");
+  
+}
