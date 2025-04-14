3d5300dc0a283e815b0d78f4f495f828cc65976a
Shoaib khan <shoaibmangal@yahoo.com>
2025-04-15 00:30:27 +0500

    add filed to js practice 09

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("images").addEventListener('click', function(e){
+    console.log("clicked inside the ul")
+    e.timeStamp()
+}, true)
+
+document.getElementById("shoaib").addEventListener('click', function(e){
+    console.log("clicked shoaib")
+}, true)
