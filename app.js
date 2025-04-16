4fed0563d44a131f37600b09017f190c5cda0c9e
Shoaib khan <shoaibmangal@yahoo.com>
2025-04-17 00:45:44 +0500

    add files to js pracrice 09

--- app.js
+++ app.js
@@ -8,41 +8,57 @@
 // }, true)
 
 
-const students = [
-    {id: "A1", name: "Ali"},
-    {id: "A2", name: "Aslam"},
-    {id: "A3", name: "Ahmad"},
-    {id: "A4", name: "shoaib"},
-    {id: "A5", name: "irfan"},
-    {id: "A6", name: "abubakkar"},
-    {id: "A7", name: "umer"},
-    {id: "A8", name: "usman"},
-    {id: "A9", name: "hassan"},
-    {id: "A10", name: "hussain"},
-];
+// const students = [
+//     {id: "A1", name: "Ali"},
+//     {id: "A2", name: "Aslam"},
+//     {id: "A3", name: "Ahmad"},
+//     {id: "A4", name: "shoaib"},
+//     {id: "A5", name: "irfan"},
+//     {id: "A6", name: "abubakkar"},
+//     {id: "A7", name: "umer"},
+//     {id: "A8", name: "usman"},
+//     {id: "A9", name: "hassan"},
+//     {id: "A10", name: "hussain"},
+// ];
 
-function searchStudent(event){
-  event.preventDefault();
-  const idField = document.getElementById("id-field");
-  const showResultElement = document.getElementById("show-result");
-  const foundStudent = students.find(function(item){
-    if(item.id === idField.value){
-        returntrue;
-    } else {
-        false;
-    }
-  });
-  if (foundStudent){
-    showResultElement.innerText = "id found";
-  } else {
-    showResultElement.innerText = "id not found"
-  }
+// function searchStudent(event){
+//   event.preventDefault();
+//   const idField = document.getElementById("id-field");
+//   const showResultElement = document.getElementById("show-result");
+//   const foundStudent = students.find(function(item){
+//     if(item.id === idField.value){
+//         returntrue;
+//     } else {
+//         false;
+//     }
+//   });
+//   if (foundStudent){
+//     showResultElement.innerText = "id found";
+//   } else {
+//     showResultElement.innerText = "id not found"
+//   }
 
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+//    for(let j = 1; j <= 10; j++)
+//     console.log(i * j + " = " + i + i+j)
+// }
+
+function increment() {
+    count++;
+    console.log(count++);
 }
 
-for(let i = 1; i <= 10; i++){
-    console.log(i)
-   for(let j = 1; j <= 10; j++)
-    console.log(i * j + " = " + i + i+j)
+function decrement() {
+    count--;
+    console.log(count--);
+}
+
+function popup(){
+    console.log(popup)
+    increment();
+    decrement();
 }
 
